fa18e8b23f55843ed9021cd5f3b24a527cedd53c
JairMarquez1 <a19310186@ceti.mx>
2020-07-25 18:02:22 -0500

    Version 1

---
+++ main.js
@@ -0,0 +1,26 @@
+let L = [1,2,3,4,5,6,7,8,9];
+let E = [1,2,3,4,5,6,7,8,9];
+let V = [1,2,3,4,5,6,7,8,9];
+let res = ['','','','','','','','',''];
+
+function start(){
+    for (var i = 1; i < 10 ; i++){
+        L[i] = document.getElementById("L"+i);
+        E[i] = document.getElementById("E"+i);
+        V[i] = document.getElementById("V"+i);
+    }
+}
+
+function selection(element){
+    if (!element.style.backgroundColor){
+        element.style.backgroundColor = "rgba(95, 158, 160, 0.35)"
+        let index = parseInt(element.id.slice(1)) - 1;
+        console.log(index);
+        res[index] = element.id.slice(0,1);
+        console.log(res);
+    }
+    else
+    element.style.backgroundColor = null;
+}
+
+window.addEventListener("load",start,false);
