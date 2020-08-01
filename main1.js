a5532fa0f514dffea2586fdada1abb5f5f2c61d2
JairMarquez1 <a19310186@ceti.mx>
2020-08-01 06:04:48 -0500

    Version 24

--- main1.js
+++ main1.js
@@ -1,31 +1,31 @@
 let res = ['_','_','_','_','_','_','_','_','_'];
 let quantity = localStorage.getItem('quantity');
-let name;
+let name = localStorage.getItem("alias");
 let combinations = false;
 var aux;
 
 function start(){
     recovername();
+    display = document.getElementById("display");
     let width = document.getElementsByClassName('quiniela')[0].offsetWidth;
     document.getElementById("quiniela").style.height = width*0.9375 + "px";
     let container = document.getElementById("text");
     container.innerHTML = res.join("\xa0\xa0");
-    let quantity = localStorage.getItem('quantity');
-    /*if (quantity>999){deleteall();}*/
-    name = localStorage.getItem("alias");
     if (quantity)
-    document.querySelector('.botonenviar span').textContent = quantity;
+        document.querySelector('.botonenviar span').textContent = quantity;
     let results = localStorage.getItem("results");
     if (results){
         results = results.split("*");
         for (var i = 0; i < quantity; i++){
-            if (results[i] != undefined){
-                document.getElementById("display").innerHTML += `<div>`+ results[i] +`</div>`;} 
+            if (results[i] && results[i] != undefined){
+                display.innerHTML += `<div>`+ results[i] + `</div>`;} 
             else
                 break;
     }
     document.getElementById("total").innerHTML = "Total: $" + quantity*25 +"\n";
-}  }
+    display.lastElementChild.innerHTML += `<button id="undo" onclick="remove()"><ion-icon name="arrow-undo-outline"></ion-icon></button>`;
+}  
+}
 
 function selection(element){
     let index = parseInt(element.id.slice(1)) - 1;
@@ -76,6 +76,7 @@ function number(){
 function result(){
     results = localStorage.getItem("results");
     name = document.getElementById("nombre").value;
+    name  = name.split('*').join('');
     if (results){
         /*localStorage.setItem('results', results + "%0D" + res.join("%20%20"));*/
         if (aux > 1)
@@ -92,23 +93,28 @@ function result(){
 }
 
 function send(){
-    if (!quantity)
+    if (!quantity || quantity < 1)
         save();
-    else{
+    if (quantity > 0){
     let whatsapptext = res.join("%20%20")
     whatsapptext = encodeURI(localStorage.getItem("results"));
     whatsapptext = whatsapptext.split('*').join('%0D');
-    window.location.href = "https://wa.me/523318325850?text="+whatsapptext;}
+    window.location.href = "https://wa.me/523317816346?text="+whatsapptext;}
 }
 
 function save(){
+    name = document.getElementById("nombre").value;
     if (res.join("\xa0\xa0").includes("_"))
         alert("Debes llenar todas las casillas");
+    else if (!name){
+        alert("Debes elegir un nombre");
+        return 0;}
     else{
         if (combinations)
             calculate();
         else
             number();
+
         result();
         updatedisplay();
         clean();}
@@ -132,11 +138,15 @@ function clean(){
 }
 
 function updatedisplay(){
+    let display = document.getElementById("display");
+    if (quantity > 1)
+    display.lastElementChild.removeChild(display.lastElementChild.lastElementChild);
     if (aux > 1)
-        document.getElementById("display").innerHTML += `<div>` +  res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*\xa0\xa0" + `</div>`;
+        display.innerHTML += `<div>` +  res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*\xa0\xa0" + `</div>`;
     else
-    document.getElementById("display").innerHTML += `<div>` +  res.join("\xa0\xa0") + "\xa0\xa0" + name + "*\xa0\xa0" + `</div>`;
+    display.innerHTML += `<div>` +  res.join("\xa0\xa0") + "\xa0\xa0" + name + "*\xa0\xa0" + `</div>`;
     document.getElementById("total").innerHTML = "Total: $" + quantity*25;
+    display.lastElementChild.innerHTML += `<button id="undo" onclick="remove()"><ion-icon name="arrow-undo-outline"></ion-icon></button>`;
     aux = 1;
 }
 
@@ -152,7 +162,7 @@ function clearname(){
 
 function allowcombination(){
     if (!combinations) 
-        combinations = confirm("¿Desea registras quinielas multiples?");
+        combinations = confirm("¿Desea registrar quinielas multiples?");
     else
         combinations= false;
     document.getElementById("checkcombinaciones").checked = combinations;
@@ -202,6 +212,50 @@ function costoactual(){
         }
         document.getElementById("costo").innerHTML = "Costo: $" + aux2*25;
         document.getElementById("numquinielas").innerHTML = aux2 + " Quiniela(s)"
+    }
+}
+function remove(){
+    if (quantity > 0){
+        x = localStorage.getItem("results");
+        x = removeLastLine(x);
+        localStorage.setItem("results", x);
+
+        let display = document.getElementById('display');
+        last = display.lastElementChild.textContent;
+        last = last.split("\xa0\xa0");
+        last [0] = last[0].split('\n').join('');
+
+        let aux3 = 1;
+        for (var i=0;i<9;i++){
+            aux3*= last[i].length;
+            }
+        if (quantity == 1)
+            localStorage.setItem("results","");
+        quantity -= aux3;
+    
+        display.removeChild(display.lastElementChild);
+
+        let container = document.getElementById("text");
+        container.innerHTML = res.join("\xa0\xa0");
+
+        let results = localStorage.getItem("results");
+        results = results.split("*");
+
+        document.querySelector('.botonenviar span').textContent = quantity;
+        document.getElementById("total").innerHTML = "Total: $" + quantity*25 +"\n";
+
+        localStorage.setItem('quantity', quantity);
+
+        if (display.lastElementChild != null)
+            display.lastElementChild.innerHTML += `<button id="undo" onclick="remove()"><ion-icon name="arrow-undo-outline"></ion-icon></button>`;
+}
+}
+
+function removeLastLine(){
+    if(x.lastIndexOf("\n")>0) {
+        return x.substring(0, x.lastIndexOf("\n"));
+    } else {
+        return x;
     }
 }
 
